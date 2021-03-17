20a7f1b8563d7c8e45bb481a357b71f8595bb5d4
KLYE-Dev <kylejrturnbull@gmail.com>
2021-03-17 05:35:25 -0600

    init push

--- libs/accounts.js
+++ libs/accounts.js
@@ -1,161 +1,141 @@
-var RpcInfo = require('burrow-rpcinfo')
-var fs = require('fs')
-var path = require('path')
-var schema = require('./schema').Schema
-var Promise = require('promise')
-var rpcInfo = null
+
+var burrowDbFactory = require('burrow-db');
+var fs              = require('fs');
+var path            = require('path'); 
+var schema          = require('./schema').Schema;
+var Promise         = require('promise');
+var accounts        = null;
 
 module.exports = class Accounts {
-  constructor(burrow_url) {
-    rpcInfo = new RpcInfo(burrow_url)
-  }
 
-  getAccounts() {
-    return new Promise(function (resolve, reject) {
-      console.log("Accounts");
-      rpcInfo.getAccounts((error, data) => {
-        if (data) {
-          resolve(data)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    constructor(connectionUrl){
+        var burrow = burrowDbFactory.createInstance(connectionUrl);
+        accounts = burrow.accounts();
+    }
+    
+    loadAccounts(){
+        return new Promise(function (resolve, reject) {
+            accounts.getAccounts((error,data)=>{
+                if(data){                                               
+                    resolve(data);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        });        
+    }
+    
+    createAccount(pass_phrase){    
+        return new Promise(function (resolve, reject) {
+            accounts.genPrivAccount(pass_phrase,(error,data)=>{
+                if(data){                                               
+                    resolve(data);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        }); 
+    }
+    
+    getBalance(address){    
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.Balance);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        }); 
+    }
 
-  createAccount(pass_phrase) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.genPrivAccount(pass_phrase, (error, data) => {
-        if (data) {
-          resolve(data)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    getSequence(address){    
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.Sequence);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        }); 
+    }
 
-  getBalance(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Balance)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    getBalance(address){        
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.Balance);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        });
+    }
+    
+    getPermissions(address){        
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.Permissions);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        });
+    }
+    
+    getStorageRoot(address){        
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.StorageRoot);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        });
+    }
 
-  getAccountInfo(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    getCode(address){    
+        return new Promise(function (resolve, reject) {
+            accounts.getAccount(address,(error,data)=>{
+                if(data){                                               
+                    resolve(data.Account.Code);
+                }    
+                else{
+                    reject(error);   
+                } 
+            })
+        });
+    }
 
-  getSequence(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Sequence)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    getDefaultAccounts(){
+        return new Promise(function (resolve, reject) {
+            try{  
+                let filePath = path.normalize(__dirname + schema.template + schema.account_list);      
+                if (fs.existsSync(filePath)) {
+                    let account_list = JSON.parse(fs.readFileSync(filePath, 'utf-8'));                                       
+                    resolve(account_list);
+                }
+                else{
+                    throw ('The file does not exist : \n' + filepath);
+                }
+            }    
+            catch(ex){
+                reject(ex);   
+            } 
+        });
+    }
 
-  getBalance(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Balance)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getPermissions(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Permissions)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getStorageRoot(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.StorageRoot)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getCode(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getAccount(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Code)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getDumpStorage(address) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getDumpStorage(address, (error, data) => {
-        if (data) {
-          resolve(data.Account.Code)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getStorage(address, key) {
-    return new Promise(function (resolve, reject) {
-      rpcInfo.getStorage(address, key, (error, data) => {
-        if (data) {
-          resolve(data.Account.Code)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  getDefaultAccounts() {
-    return new Promise(function (resolve, reject) {
-      try {
-        let filePath = process.env.HOME + "/burrow/.keys/data";
-        console.log('Key files path: \n' + filePath)
-        fs.readdir(filePath, function(err, items) {               
-          for (var i=0; i<items.length; i++) {
-              console.log(fs.readFileSync(filePath+'/'+items[i], 'utf8'));
-          }
-      });
-      } catch (ex) {
-        reject(ex)
-      }
-    })
-  }
 }
+
+
+
--- libs/project.js
+++ libs/project.js
@@ -1,80 +1,91 @@
 'use strict'
 
-var fs = require('fs')
-var Promise = require('promise')
-var Schema = require('./schema').Schema
+var fs      = require("fs");
+var Promise = require("promise");
+var Schema  = require("./schema").Schema;
 
 module.exports = class Project {
-  constructor () {
 
-  }
+    constructor(){
 
-  createSchema () {
-    return new Promise(function (fulfil, reject) {
-      var callback = function (error, data) {
-        if (error) { reject(error) }
-      }
-      try {
-        fs.copyFileSync(Schema.snack_path + Schema.template + Schema.config_file, Schema.project_path + Schema.config_file)
-        fs.mkdirSync(Schema.project_path + Schema.build)
-        fs.mkdirSync(Schema.project_path + Schema.contracts)
-        fs.mkdirSync(Schema.project_path + Schema.migration)
-        fs.mkdirSync(Schema.project_path + Schema.test)
-        fs.mkdirSync(Schema.project_path + Schema.accounts)
-        fs.mkdirSync(Schema.project_path + Schema.transactions)
+    }
 
-        fulfil()
-      } catch (ex) {
-        console.log(ex)
-        reject(ex)
-      }
-    })
-  }
+    createSchema(){
+        return new Promise(function (fulfil,reject){
+            var callback = function(error , data){
+                if(error)
+                    reject(error);
+            }
+            try{
+                fs.copyFileSync(Schema.snack_path +Schema.template + Schema.config_file, Schema.project_path + Schema.config_file);
+                fs.mkdirSync(Schema.project_path + Schema.build);
+                fs.mkdirSync(Schema.project_path + Schema.contracts);
+                fs.mkdirSync(Schema.project_path + Schema.migration);
+                fs.mkdirSync(Schema.project_path + Schema.test);
+                fs.mkdirSync(Schema.project_path + Schema.accounts);
+                fs.mkdirSync(Schema.project_path + Schema.transactions);
+                fs.copyFileSync(Schema.snack_path +Schema.template + Schema.account_list, Schema.project_path + Schema.accounts + Schema.account_list);
+                fs.copyFileSync(Schema.snack_path +Schema.template + Schema.default_account, Schema.project_path + Schema.accounts + Schema.default_account);
 
-  listContracts () {
-    return new Promise(function (fulfil, reject) {
-      var dir = require('node-dir')
-      var path = require('path')
-      try {
-        let dirPath = Schema.project_path + Schema.contracts
-        dir.files(dirPath, function (err, files) {
-          if (err) reject(err)
+                fulfil();
+            }
+            catch(ex){
+                console.log(ex);
+                reject(ex);
+            }
+    });
+    }
 
-          files = files.filter(function (file) {
-            return path.extname(file) == '.sol' && path.basename(file)[0] != '.'
-          })
-          if (files.length == 0) { reject('There is no .sol file in this directory!!') }
+    listContracts() {
+        return new Promise(function (fulfil,reject){
+            var dir = require("node-dir");
+            var path = require("path");
+            try{
+                let dirPath = Schema.project_path + Schema.contracts;
+                dir.files(dirPath, function(err, files) {
+                    if (err) reject(err);
+                      
+                    files = files.filter(function(file) {
+                        return path.extname(file) == ".sol" && path.basename(file)[0] != ".";                
+                    });
+                    if(files.length ==0)
+                        reject("There is no .sol file in this directory!!");
+    
+                    fulfil(files);
+                });
+            }
+            catch(ex){
+                reject(ex);
+            }
+        });           
+    }
 
-          fulfil(files)
-        })
-      } catch (ex) {
-        reject(ex)
-      }
-    })
-  }
+    getContractsNames() {        
+        return new Promise(function (fulfil,reject){
+            var dir = require("node-dir");
+            var path = require("path");
+            try{                
+                let dirPath = Schema.project_path + Schema.contracts;
+                dir.files(dirPath, function(err, files) {
+                    if (err) reject(err);
+                      
+                    files = files.filter(function(file) {
+                        return path.extname(file) == ".sol" && path.basename(file)[0] != ".";                                      
+                    });
 
-  getContractsNames () {
-    return new Promise(function (fulfil, reject) {
-      var dir = require('node-dir')
-      var path = require('path')
-      try {
-        let dirPath = Schema.project_path + Schema.contracts
-        dir.files(dirPath, function (err, files) {
-          if (err) reject(err)
+                    if(files.length ==0) 
+                        reject("There is no sol file in this directory!!");
 
-          files = files.filter(function (file) {
-            return path.extname(file) == '.sol' && path.basename(file)[0] != '.'
-          })
+                    for(var i=0 ; i< files.length ; i++)
+                        files[i] = path.parse(files[i]).name;
 
-          if (files.length == 0) { reject('There is no sol file in this directory!!') }
+                    fulfil(files);
+                });
+            }
+            catch(ex){
+                reject(ex);
+            }
 
-          for (var i = 0; i < files.length; i++) { files[i] = path.parse(files[i]).name }
-
-          fulfil(files)
-        })
-      } catch (ex) {
-        reject(ex)
-      }
-    })
-  }
+        });       
+    }
 }
